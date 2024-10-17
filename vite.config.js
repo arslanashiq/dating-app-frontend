e9f8c51942a9702a1172775878088832398af042
arslanashiq <arslan@galaxyitc.com>
2024-10-17 10:56:24 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/app/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx'],
